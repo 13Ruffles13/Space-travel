04385c04e901c022933ce3505732805500d4dc5c
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2023-08-28 13:35:23 -0700

    completedTask

--- App/src/App.js
+++ App/src/App.js
@@ -1,9 +1,21 @@
 import { useContext } from "react";
 import { BrowserRouter } from "react-router-dom";
+
 import styles from "./App.module.css";
+import { LoadingContext } from "./context/LoadingProvider";
 
 function App() {
-  return <h1>Space Travel</h1>;
+  const { isLoading } = useContext(LoadingContext);
+  return (
+    <>
+      <BrowserRouter>
+        <div className={styles["app"]}>
+          <header className={styles["app__header"]}></header>
+        </div>
+      </BrowserRouter>
+      <h1>Space Travel</h1>
+    </>
+  );
 }
 
 export default App;
--- TaskTimestamps.md
+++ TaskTimestamps.md
@@ -2,4 +2,6 @@
 
 ## August 28, 2023
 
-- [x] 12:00 PM - The entry point (index.js), which renders the App component, should be wrapped with a loading provider. The loading provider creates a context for loading.
+- [x] 12:00 PM to 1:00 PM - The entry point (index.js), which renders the App component, should be wrapped with a loading provider. The loading provider creates a context for loading.
+
+- [x] 1:00 PM to 1:30 PM - Implementation of NavBar file into component structure folder for Navigation. Added BrowserRouter to my main App file component.
