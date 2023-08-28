3bcbb6819c95ad69f7b9d78f2f71613bf403de6b
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2023-08-28 13:09:07 -0700

    hooks

--- App/src/App.js
+++ App/src/App.js
@@ -1,8 +1,9 @@
-function App ()
-{
-  return (
-    <h1>Space Travel</h1>
-  );
+import { useContext } from "react";
+import { BrowserRouter } from "react-router-dom";
+import styles from "./App.module.css";
+
+function App() {
+  return <h1>Space Travel</h1>;
 }
 
 export default App;
